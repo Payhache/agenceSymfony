3dff3f7c6cefffbc75ba76ecd1cbd5188539aabe
payhache <payhache@gmail.com>
2020-08-20 11:39:44 +0200

    ajout suppression activité

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -21,14 +21,37 @@ class ActivityController extends AbstractController
             'activities' => $activities
         ]);
     }
+    /**
+     * @Route("/activity/add", name="activity_add")
+     */
+    public function addActivity() {
+        $newActivities = ['patin a glace', 'chasse au trésor', 'attrappe maman'];
+        $entityManager = $this->getDoctrine()->getManager();
+        foreach ($newActivities as $act) {
+           $activity = new Activity();
+           $activity->setName($act);
+           $entityManager->persist($activity);
+        }
+        $entityManager->flush();
+        return $this->render('activity/activity_add.html.twig');
+    }
 
     /**
      * @Route("/activity/{activity}", name="activity_display")
      */
     public function activityDisplay(Activity $activity) {
-        return $this->render('activity/activity_add.html.twig', [
+        return $this->render('activity/activity_detail.html.twig', [
             'activity' => $activity
         ]);
-
     }
+        /**
+     * @Route("/activity/{activity}/delete", name="activity_delete")
+     */
+    public function activityDelete(Activity $activity) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($activity);
+        $entityManager->flush();
+        return $this->render('activity/activity_delete.html.twig');
+    }
+
 }
